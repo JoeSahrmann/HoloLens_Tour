b28f4aa78ed370ae68297f720b487e0ffd79cf41
JoeSahrmann <jxsmgb@mail.missouri.edu>
2021-12-02 17:41:26 -0600

    Audio Buttons Added
    I spent the last 4 hours working on this project again today. What I did was first go through all of the mizzou websites I could find. With the intentions of finding new header photos for the cards. I found some great images and used them on all the cards keeping a consistency with the number category. Meaning that if the third category on one card was the columns it would be that on every card. This helps the viewer know that they have switched cards and what number category they are on. That is if they can't tell by the title and radio button being filled. The next thing I did was go through all of the materials and change them to be a Mixed reality / standard material. This seemed to fix the dark tint issue that I was facing yesterday. However the colors still seem off and I do not know what to do about it. The next thing that I did was add to the scrip all of the text that the other members have provided me with. Here you will see that it is not all filled out that is because they are still working on it. Then I figured out that I would only need to be changing two photos on the Lab menu dynamically. With these photos being the lab header of the general tab and the director headshot on the director tab. I added in the image objects into the script and had their material update based on what lab was selected. After this I then started making all of the audio buttons. But before I could start I had to go to word and get the audio on and off symbols make them mizzou gold, then remove the background from the images. Import them into unity and turn them into a cut out material. From there I created many prototypes of how the the button object should be structured. Then final settled on two buttons with an icon outside of the button. Then these buttons are found inside of two parents call on and off. So in on there is an on button and icon and same for off but with the off button and icon. When one is clicked the other is turned off. The next step is to get these buttons to play audio based on the lab that is selected. As well as, be able to stop the audio before it is done.

--- Beta-UI/Assets/Scripts/DynamicStops.cs
+++ Beta-UI/Assets/Scripts/DynamicStops.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class DynamicStops : MonoBehaviour
@@ -20,16 +21,29 @@ public class DynamicStops : MonoBehaviour
     //General Cards
     public TextMeshProUGUI GeneralBlurb;
     public TextMeshProUGUI GeneralDetails;
+    public Image GeneralHeaderPhoto;
     //News Cards
     public TextMeshProUGUI NewsBlurb;
     public TextMeshProUGUI NewsDetails;
     //Director Cards
     public TextMeshProUGUI DirBlurb;
     public TextMeshProUGUI DirDetails;
+    public Image DirectorPhoto;
     //Faculty Cards
     public TextMeshProUGUI FacBlurb;
     public TextMeshProUGUI FacDetails;
-
+    //all header materials
+    public Material Asphalt_Header;
+    public Material CERI_Header;
+    public Material SPOT_Header;
+    public Material VR_Header;
+    public Material ThreeD_Header;
+    //all director materials
+    public Material Asphalt_Director;
+    public Material CERI_Director;
+    public Material SPOT_Director;
+    public Material VR_Director;
+    public Material ThreeD_Director;
     // Start is called before the first frame update
     void Start()
     {
@@ -68,6 +82,7 @@ public class DynamicStops : MonoBehaviour
             CafeMenu.SetActive(false);
             //turn on Library Menu
             LabMenu.SetActive(true);
+
         }
     }
     //populate the lab menu with the correct text
@@ -114,6 +129,8 @@ public class DynamicStops : MonoBehaviour
         string Asphalt_F_Blurb = "James Meister and Henry C. Brown are currently assisting Bill Buttlar with the Mizzou Asphalt Pavement and Innovations Lab. Select Read more to learn about the faculty.";
         string Asphalt_F_Detials = "James Meister is responsible for purchase and installation of the testing equipment in MAPIL. As well as, training all student researchers on proper equipment usage and applicable test standards while conducting field visits for asphalt sampling.\nHenry C. Brown is responsible for asset management for local governments, development of a work zone safety assessment tool, calibration of Highway Safety Manual, evaluating work zone sign configurations, evaluating J-turn intersection designs, and data collection for bicycle treatment evaluation.";
         //now assing them to the TMPGUI text on the cards
+        //Header Photo
+        GeneralHeaderPhoto.material = Asphalt_Header;
         //title
         Title.text = Asphalt_Title;
         //general cards
@@ -125,6 +142,8 @@ public class DynamicStops : MonoBehaviour
         //director cards
         DirBlurb.text = Asphalt_D_Blurb;
         DirDetails.text = Asphalt_D_Detials;
+        //director Photo
+        DirectorPhoto.material = Asphalt_Director;
         //facult cards
         FacBlurb.text = Asphalt_F_Blurb;
         FacDetails.text = Asphalt_F_Detials;
@@ -148,6 +167,8 @@ public class DynamicStops : MonoBehaviour
         string SPOT_F_Blurb = "";
         string SPOT_F_Detials = "";
         //now assing them to the TMPGUI text on the cards
+        //Header Photo
+        GeneralHeaderPhoto.material = SPOT_Header;
         //title
         Title.text = SPOT_Title;
         //general cards
@@ -159,6 +180,8 @@ public class DynamicStops : MonoBehaviour
         //director cards
         DirBlurb.text = SPOT_D_Blurb;
         DirDetails.text = SPOT_D_Detials;
+        //director Photo
+        DirectorPhoto.material = SPOT_Director;
         //facult cards
         FacBlurb.text = SPOT_F_Blurb;
         FacDetails.text = SPOT_F_Detials;
@@ -170,18 +193,20 @@ public class DynamicStops : MonoBehaviour
         //title
         string CERI_Title = "Cyber Education Research & Infrastructure";
         //general cards
-        string CERI_G_Blurb = "";
-        string CERI_G_Detials = "";
+        string CERI_G_Blurb = "CERI is a multi-disciplinary center formed to foster information technology innovations in areas of Cyber, Artificial Intelligence and Big Data Analytics to meet the needs of the nation in information security and cyber defense.";
+        string CERI_G_Detials = "CERI Center was formed in 2019 with support from a highly competitive 2019 UM System Tier 1 grant, guided by an existing seed investment (serving as a cost-share commitment for the proposed project duration) of “Cyber Education and Research Initiative” at the MU College of Engineering. The CERI Center builds on research strengths, and a survey of cyber needs across the UM System/peer universities, and funding priorities of industry/federal agencies.";
         //news cards
-        string CERI_N_Blurb = "";
-        string CERI_N_Detials = "";
+        string CERI_N_Blurb = "The CERI lab studies cyber security areas including: infrastructure, vision, intelligence, and training. Their research has led to many newsworthy breakthroughs over the last three years in each of their focus areas.";
+        string CERI_N_Detials = "Cyber vision:A CAVE is a small facility that provides users with an immersive virtual environment. It consists of adjustable walls, a floor and motion sensors that track movement. Unlike other types of virtual reality, users do not have to wear bulky headsets to experience their 3D surroundings.\nCyber intelligence: A team of Mizzou Engineers is turning to artificial intelligence (AI) to help grow and control large quantities of carbon nanotubes—tiny, cylinder-shaped molecules made of rolled sheets of carbon.";
         //director cards
-        string CERI_D_Blurb = "";
-        string CERI_D_Detials = "";
+        string CERI_D_Blurb = "Prasad Calyam is an associate professor in the Department of Electrical Engineering and Computer Science, director of the Mizzou CERI, and a core faculty member in the MU Institute for Data Science & Informatics.";
+        string CERI_D_Detials = " His research and development areas of interest include distributed and cloud computing, computer networking, networked-multimedia applications and cyber security. He has published more than 145 papers in various conference and journal venues. As the principal investigator, he has successfully led teams of graduate, undergraduate and postdoctoral fellows in numerous federal-, state- and industry-sponsored research and development projects.";
         //faculty cards
         string CERI_F_Blurb = "";
         string CERI_F_Detials = "";
         //now assing them to the TMPGUI text on the cards
+        //Header Photo
+        GeneralHeaderPhoto.material = CERI_Header;
         //title
         Title.text = CERI_Title;
         //general cards
@@ -193,6 +218,8 @@ public class DynamicStops : MonoBehaviour
         //director cards
         DirBlurb.text = CERI_D_Blurb;
         DirDetails.text = CERI_D_Detials;
+        //director Photo
+        DirectorPhoto.material = CERI_Director;
         //facult cards
         FacBlurb.text = CERI_F_Blurb;
         FacDetails.text = CERI_F_Detials;
@@ -204,18 +231,20 @@ public class DynamicStops : MonoBehaviour
         //title
         string VR_Title = "Virtual Reality lab";
         //general cards
-        string VR_G_Blurb = "";
+        string VR_G_Blurb = "Mizzou’s Virtual Reality Lab is a state of the art laboratory with several computer workstations and VR play spaces for students to develop and explore virtual reality technology.";
         string VR_G_Detials = "";
         //news cards
-        string VR_N_Blurb = "";
-        string VR_N_Detials = "";
+        string VR_N_Blurb = "Mizzou Engineering students have created an interactive art exhibit that will allow visitors to view paintings without ever stepping inside a museum. The exhibit is being created digitally for a virtual reality (VR) experience.";
+        string VR_N_Detials = "Students created various lighting effects for each piece. Charles Sielert, a recent IT graduate who is volunteering for the IT Program, took the lead on modeling the rooms, which also feature digital benches and elaborate flooring. The students also matched wall colors to the themes of the artwork; for instance, a room showcasing paintings of Missouri’s rivers is blue, while an agricultural-themed section is green.";
         //director cards
-        string VR_D_Blurb = "";
+        string VR_D_Blurb = "Fang Wang is a professor in the IT Program at the University of Missouri. Wang has worked as a senior software engineer in research and development at Ansys Inc. and Motorola Inc.";
         string VR_D_Detials = "";
         //faculty cards
         string VR_F_Blurb = "";
         string VR_F_Detials = "";
         //now assing them to the TMPGUI text on the cards
+        //Header Photo
+        GeneralHeaderPhoto.material = VR_Header;
         //title
         Title.text = VR_Title;
         //general cards
@@ -227,6 +256,8 @@ public class DynamicStops : MonoBehaviour
         //director cards
         DirBlurb.text = VR_D_Blurb;
         DirDetails.text = VR_D_Detials;
+        //director Photo
+        DirectorPhoto.material = VR_Director;
         //facult cards
         FacBlurb.text = VR_F_Blurb;
         FacDetails.text = VR_F_Detials;
@@ -250,6 +281,8 @@ public class DynamicStops : MonoBehaviour
         string ThreeD_F_Blurb = "";
         string ThreeD_F_Detials = "";
         //now assing them to the TMPGUI text on the cards
+        //Header Photo
+        GeneralHeaderPhoto.material = ThreeD_Header;
         //title
         Title.text = ThreeD_Title;
         //general cards
@@ -261,6 +294,8 @@ public class DynamicStops : MonoBehaviour
         //director cards
         DirBlurb.text = ThreeD_D_Blurb;
         DirDetails.text = ThreeD_D_Detials;
+        //director Photo
+        DirectorPhoto.material = ThreeD_Director;
         //facult cards
         FacBlurb.text = ThreeD_F_Blurb;
         FacDetails.text = ThreeD_F_Detials;
